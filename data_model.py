1e8026a2cc4e31e8af0fa60fdd9a8c036c88c738
Ola Ibrahim <92628525+olaib@users.noreply.github.com>
2023-08-23 20:57:09 +0300

    some fixes
    Add suffix and prefix dictionary for Levenshtein calculation

--- data_model.py
+++ data_model.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
+from typing import Tuple, List, Any
+import Levenshtein
 
 MAX_RESULTS = 5
+
+SUFFIX_DICT = {}
+PREFIX_DICT = {}
 
 
 @dataclass
@@ -12,71 +17,111 @@ class AutoCompleteData:
 
 
 class TrieNode:
-    def __init__(self, sentence: str):
+    def __init__(self, word: str, depth_from_root: int = 0):
         self.children = {}
         self.is_end = False
-        self.sentence = sentence
+        self.word = word
         self.filename = ""
         self.count = 0
-        self.min_depth_to_leaf = 0  # for optimization of the dfs traversal
+        self.start_from = {}  # to store a pointers to the root of the subtree that current node is contained in
+
+    def update_end_of_line_chain(self, file_name: str) -> None:
+        self.filename = file_name  # update the file name
+        self.count += 1  # occurrences of the same sentence
+        self.is_end = True  # end of line
 
 
 class TrieTree:
     def __init__(self):
         self.root = TrieNode("")
+        # dictionary that store the pointer for places of the words in tree
+        self.pointer_dict_to_words = {}
+        # self.starts_and_ends_with = {}
 
-    # todo: update min_depth_to_leaf
     def insert(self, line: str, filename: str) -> None:
         node = self.root
         # split the sentence to words and iterate over the words and insert to the tree as chain
-        for word in line.split():
-            word = word.lower()
-            if word in node.children:
-                node.children[word] = TrieNode(word)
+        words = list(map(lambda word: word.lower(), line.split()))
+        if not words:
+            return
+        self.starts_and_ends_with.setdefault(words[0], set())
+        for i, word in enumerate(words, start=1):
+            node = node.children.setdefault(word, TrieNode(word, i))
+            # store the pointer to the node in the dictionary as set
+            self.pointer_dict_to_words.setdefault(word, set()).add(node)
+            if not word == words[0]:
+                self.starts_and_ends_with[words[0]].add(node)
+
+            # print(self.pointer_dict_to_words)
+            # print(self.starts_and_ends_with)
+            # Prefix and suffix dict for levenshtein distance
+            for i in range(len(word)):
+                SUFFIX_DICT.setdefault(word[i:], set()).add(node)
+                PREFIX_DICT.setdefault(word[:i + 1], set()).add(node)
+
+    def get_all_sentences(self, node: TrieNode, prefix: str, output: list[AutoCompleteData], k: int = MAX_RESULTS):
+        if len(output) >= k:
+            return
+        if node.is_end:
+            output.append(AutoCompleteData(prefix, node.filename, 0, 0))
+        for child in node.children.values():
+            if len(output) < k:
+                self.get_all_sentences(child, f"{prefix} {child.word}", output, k)
             else:
-                new_node = TrieNode(word)
-                node.children[word] = new_node
-                node = new_node
-        # if already exists in the tree so update the node
-        node.is_end = True  # end of line
-        node.count += 1  # same line already exists
-        node.filename = filename
+                break
 
     @staticmethod
-    def check_if_prefix_exists(self, node: TrieNode, prefix_words: list[str]) -> tuple[bool, str]:
-        last_word_iterated = ""
-        for i, word in enumerate(prefix_words):
-            word = word.lower()
+    def get_node_by_prefix(node: TrieNode, prefix: str) -> tuple[TrieNode, bool]:
+        """
+        this function iterate on chain of words based on the prefix and return the last node in the chain
+        :param node: the root node
+        :param prefix: the prefix to search
+        :return: tuple of the last node in the chain and boolean if the prefix is in the tree
+        """
+        is_exists = True
+        for word in prefix.split():
             if word in node.children:
-                word = word.lower()
-                last_word_iterated = word
-                if node.children[word].min_depth_to_leaf < i:
-                    return False, last_word_iterated
                 node = node.children[word]
-            else:
-                return False, last_word_iterated
-        return True, last_word_iterated
+            else:  # if the word is not in the tree
+                is_exists = False
+        return node, is_exists
 
-    def search_and_get_k_suggestions(self, sentence: str, k=MAX_RESULTS) -> list[AutoCompleteData]:
-        result_suggestions = []
+    def search_and_get_k_suggestions(self, sentence: str, last_word: str, k=MAX_RESULTS) -> list[AutoCompleteData]:
+        result_suggestions: list[AutoCompleteData] = []
         # first check if the sentence is in the tree as prefix
-        prefix_words = sentence.split()
+        prefix_words = [word.lower() for word in sentence.split()]
         node = self.root
-        last_word_iterated, is_prefix_in_tree = self.check_if_prefix_exists(node, prefix_words)
+        # starts_and_ends_with = self.starts_and_ends_with.get(prefix_words[0], set())
+        # ends_with = self.starts_and_ends_with.get(last_word, set())
+        # for v in starts_and_ends_with:
+        #     if v in ends_with and v.depth_from_root == len(prefix_words):
+        #         print(v.word, v.filename,v.depth_from_root)
 
+        node = get_last_node_by_prefix(node, prefix_words)
+        # start_nodes = self.pointer_dict_to_words.get(last_word)
+        # for start_node in start_nodes:
 
-        if is_prefix_in_tree and not last_word_iterated.is_end:
-            # make dfs iteration from the last word iterated
-            self.dfs(last_word_iterated, sentence, result_suggestions, k)
+        # print(start_nodes)
+        # if start_nodes:
+        #     for start_node in start_nodes:
+        #         ends_with = self.starts_and_ends_with.get(prefix_words[0])
+        #         matching = list(filter(lambda node: node in ends_with and node.depth_from_root == len(prefix_words),
+        #                                start_node.children.values()))
 
-
+        # print(result_suggestions)
         # if was found match add to the result list
         # check if the array has 5 elements if not continue to search
-        # searching for additional methods to complete the sentence (add, remove, replace)
+        # searching for additional methods to complete the sentence (add, remove, replace) using levenshtein distance
 
     @staticmethod
     def dfs(current_node: TrieNode, prefix: str, output: list[AutoCompleteData], k: int = MAX_RESULTS):
-        pass
+        if current_node.is_end:
+            output.append(AutoCompleteData(prefix, current_node.filename, 0, 0))
+        for child in current_node.children.values():
+            if len(output) < k:
+                TrieTree.dfs(child, f"{prefix} {child.word}", output, k)
+            else:
+                break
 
     def add_char(self, current_node: TrieNode, prefix: str, output: list[AutoCompleteData]):
         pass
@@ -86,3 +131,16 @@ class TrieTree:
 
     def remove_char(self, current_node: TrieNode, prefix: str, output: list[AutoCompleteData]):
         pass
+
+    def test(self, word):
+        print("possible chains for word:", word)
+        for i, chain in enumerate(self.pointer_dict_to_words[word]):
+            print(i, chain.word)
+
+    def get_sentence_by_node_pointer(self, node_pointer: TrieNode, offset: int) -> str:
+        """
+        get the sentence by the node pointer and offset
+        :param node_pointer: the pointer to the node
+        :param offset: the offset of the sentence in the file
+        :return: the sentence
+        """
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 from typing import List
 from data_model import AutoCompleteData, TrieTree
 
+RESET_STATE = 'Enter new sentence: '
 BASE_DIR = 'Archive'
 RESET_STATE_SYMBOL = '#'
 LOADING_AND_PREPARING_SYSTEM = 'Loading and preparing the system...'
+ENTER_SENTENCE = 'Enter your text: '
 SYSTEM_READY = 'The system is ready. Enter your text:'
 K = 5
 
@@ -17,20 +19,21 @@ def get_best_k_ֵcompletions(prefix: str) -> List[AutoCompleteData]:
 
 # init the data model(loading the data from the files and store in the data model-trie tree)
 def get_files_from_folder(folder_path: str, trie_tree: TrieTree) -> None:
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(".txt"):
-                file_path = os.path.join(root, file)
-                with open(file_path, 'r') as f:
-                    for line in f:
-                        trie_tree.insert(line, file_path)
+    # for root, dirs, files in os.walk(folder_path):
+    #     for file in files:
+    #         if file.endswith(".txt"):
+    #             file_path = os.path.join(root, file)
+    file_path = 'test.txt'
+    with open(file_path, 'r') as f:
+        for line in f:
+            trie_tree.insert(line, file_path)
 
 
 def initialize_data_model(root_folder_path: str, trie_tree: TrieTree) -> None:
-    get_files_from_folder(BASE_DIR, trie_tree)
+    get_files_from_folder(root_folder_path, trie_tree)
 
 
-def main():
+def main() -> None:
     # parser = argparse.ArgumentParser(description='AutoCompleteSystem')
     # parser.add_argument('root_folder_path', type=str, help='The root directory of the project')
     # args = parser.parse_args()
@@ -39,21 +42,23 @@ def main():
     if not os.path.exists(root_folder_path):
         print(f'The path {root_folder_path} does not exist')
         return
+
     trie_tree = TrieTree()
     print(LOADING_AND_PREPARING_SYSTEM)
     initialize_data_model(root_folder_path, trie_tree)
-    # load_and_prepare_system()
-    # start the cli
     print(SYSTEM_READY)
+    user_input = ""
     while True:
-        user_input = input()
+        current_user_input = input(ENTER_SENTENCE + user_input)
         if user_input.endswith(RESET_STATE_SYMBOL):
-            print(SYSTEM_READY)
-            continue
+            user_input = input(RESET_STATE)
+        else:
+            user_input += current_user_input
         # autocomplete_results = get_best_k_ֵcompletions(prefix)
         print(TOP_K_RESULTS)
         # print(autocomplete_results)
-        print('test')
+        last_word = user_input.split()[-1]
+        print(trie_tree.search_and_get_k_suggestions(sentence=user_input, last_word=last_word))
 
 
 if __name__ == "__main__":
